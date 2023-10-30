d2a1c0ff64b147101a734df9472f8d08e41d6a46
tuliocruz16 <tulioocruz@gmail.com>
2023-10-30 16:04:44 -0300

    Backend Project Created

---
+++ backend/src/main/java/com/blog/blogpage/BlogPageApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/blog/blogpage/BlogPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
