820ca47a6bc2085af48680a4012b023a81c52086
tuliocruz16 <tulioocruz@gmail.com>
2023-11-01 10:16:24 -0300

    DTO

--- backend/src/main/java/com/blog/blogpage/services/CategoryService.java
+++ backend/src/main/java/com/blog/blogpage/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.blog.blogpage.services;
 
+import com.blog.blogpage.dto.CategoryDTO;
 import com.blog.blogpage.entities.Category;
 import com.blog.blogpage.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +18,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 }
