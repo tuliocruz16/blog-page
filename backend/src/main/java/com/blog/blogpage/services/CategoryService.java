85c69fd37cfb7f59d773cc90b23d4976556dd859
tuliocruz16 <tulioocruz@gmail.com>
2023-11-01 08:49:14 -0300

    Database seeding

--- backend/src/main/java/com/blog/blogpage/services/CategoryService.java
+++ backend/src/main/java/com/blog/blogpage/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.blog.blogpage.entities.Category;
 import com.blog.blogpage.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +13,8 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category(name) VALUES ('Self Improvement')
+INSERT INTO tb_category(name) VALUES ('Philosophy')
+INSERT INTO tb_category(name) VALUES ('History')
